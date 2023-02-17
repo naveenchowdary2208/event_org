1f519daf4a310031a6034a9af6e37c1138697a0f
Navi22@ug1997 <naveenchodary2208@gmail.com>
2023-02-17 10:54:28 +0530

    structure

---
+++ src/main/java/com/event/org/EventOrgApplication.java
@@ -0,0 +1,13 @@
+package com.event.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrgApplication.class, args);
+	}
+
+}
